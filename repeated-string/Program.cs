68ce1ebeda42deb1c033bab80ad52dcfc4d5e8da
FrhCode <farhan7534031b@gmail.com>
2023-12-03 16:58:30 +0700

    Add project files.

---
+++ repeated-string/Program.cs
@@ -0,0 +1,29 @@
+﻿long FirstLetterCount(string text, long n)
+{
+    int stringLength = text.Length;
+    
+    long count = 0;
+    for (int i = 0; i < stringLength; i++)
+    {
+        if (text[i] == 'a')
+        {
+            count++;
+        }
+    }
+    long fullStringCount =(long) n / stringLength;
+    count *= fullStringCount;
+
+    long remainder = n % stringLength;
+    for (int i = 0; i < remainder; i++)
+    {
+        if (text[i] == 'a')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+long result = FirstLetterCount("a", 1000000000000);
+Console.WriteLine(result);
+
